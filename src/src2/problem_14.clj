d91f2c234f233d86b308ea746367f6673e99d839
rdsr <rdsr.me@gmail.com>
2009-10-02 08:26:25 +0530

    minor tweaks

--- src/src2/problem_14.clj
+++
@@ -1,6 +0,0 @@
-(apply max (map (memoize cnt-chain) (range 1 1000000)))
-
-(defn cnt-chain [n]
-  (cond (= n 1) 1
-        (even? n) (inc (recur (/ n 2)))
-        (odd? n) (inc (recur (inc  (* 3 n))))))
