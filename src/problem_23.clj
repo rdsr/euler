7146e0e0bc7676e178040a167c0683fa8734b47a
rdsr <rdsr.me@gmail.com>
2009-10-06 10:28:39 +0530

    problem_23.clj

---
+++ src/problem_23.clj
@@ -0,0 +1,28 @@
+(ns problem-23
+  (use [util]))
+
+(def abndt-map (atom (sorted-map)))
+(def abndt-sum-map (atom {}))
+
+(defn abundant? [n]
+  (if (not (or (even? n) (= (rem n 5) 0)))
+    false
+    (> (apply + (proper-divisors n)) n)))
+
+(defn fill-map []
+  (doseq [n (range 12 20162)
+          i (range 1 (/ 20161 n)) 
+          :when (and (not (@abndt-map n)) (abundant? n))]
+    (swap! abndt-map assoc (* n i) true))
+  @abndt-map)
+
+(defn problem-23 []
+  (let [abndt-nos (vec (keys (fill-map)))
+        len (count abndt-nos)]
+    (doseq [i (range 0 len) j (range i len) 
+            :when (<= (+ (get abndt-nos i) (get abndt-nos j)) 20161)]
+      (swap! abndt-sum-map assoc (+ (get abndt-nos i) (get abndt-nos j)) true))
+    (apply + (filter (complement @abndt-sum-map)
+                     (range 1 20162)))))
+
+
