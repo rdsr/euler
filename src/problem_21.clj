16881fd6b09901f342e57422c8021dc43efc7269
rdsr <rdsr.me@gmail.com>
2009-10-06 12:25:24 +0530

    problem_21.clj util.clj

--- src/problem_21.clj
+++ src/problem_21.clj
@@ -1,26 +1,22 @@
-(defn sqr [n] (* n n))
-
-(defn proper-divs [d divs no]
-  (cond
-    (>= d (sqrt no)) divs
-    :else (if (= 0 (rem no d))
-            (recur (+ d 1) (conj divs d) no)
-            (recur (+ d 1) divs no))))
-
-(def memoized-proper-divs (memoize proper-divs))
+(ns problem-21
+  (:use [util]))
 
 (defn d [n]
-  (reduce + (memoized-proper-divs 2 (list 1) n)))
+  (apply + (proper-divisors n)))
 
-;; [a da/b db]
+(def amicable-nos (atom {}))
 
-(reduce
- (fn [partial-sum [a b db]] (+ a db partial-sum))
- 0
- (filter (fn [[a b db]] (and (<= b 10000) (not= a b) (= a db)))
-         (map (fn [a]
-                (let [da (d a)
-                      db (d da)]
-                  (vector a da db)))
-              (range 1 10001))))
+(defn amicable? [a]
+  (if (@amicable-nos a) true
+      (let [b (d a)
+            db (d b)]
+        (if (and (not= a b) (= a db))
+          (do (swap! amicable-nos assoc b true) true)
+          false))))
 
+(defn problem-21 []
+  (apply 
+   +
+   (filter amicable? (range 1 10000))))
+
+(problem-21)
--- src/util.clj
+++ src/util.clj
@@ -1,8 +1,23 @@
 (ns util)
 
-(defn divisors [n]
-  (loop [i 1 nos ()]
+;; (defn x-divisors [n]
+;;   (loop [i 1 nos ()]
+;;     (cond
+;;       (> i (Math/sqrt n)) nos
+;;       (= (rem n i) 0) 
+;;         (let [n-by-i (/ n i)]
+;;           (if (= i n-by-i)
+;;             (recur (inc i ) (cons i nos))
+;;             (recur (inc i) (cons i (cons (/ n i) nos)))))
+;;       :else (recur (inc i) nos))))
+
+(defn proper-divisors [n]
+  (loop [i 2 nos '(1)]
     (cond
       (> i (Math/sqrt n)) nos
-      (= (rem n i) 0) (recur (inc i) (cons i (cons (/ n i) nos)))
+      (= (rem n i) 0) 
+        (let [n-by-i (/ n i)]
+          (if (= i n-by-i)
+            (recur (inc i ) (cons i nos))
+            (recur (inc i) (cons i (cons (/ n i) nos)))))
       :else (recur (inc i) nos))))
