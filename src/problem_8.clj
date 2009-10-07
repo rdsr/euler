d1a4babf4eb6bd92cb7855e2eb01ea0fc9c0fc72
rdsr <rdsr.me@gmail.com>
2009-10-07 20:13:04 +0530

    problem 8 optimized

--- src/problem_8.clj
+++ src/problem_8.clj
@@ -1,30 +1,15 @@
-(defn flatmap [proc coll] (reduce into '() (map proc coll)))
+(ns problem-8)
 
-(defn all-groups-of [n coll]
-  (defn collect [lst1 lst2]
-    (cond (empty? lst1) lst2
-          :else (conj (collect (rest lst1) lst2)
-                      (first lst1))))
+(defn- max-prdt [son]
+  (apply
+   max
+   (map #(apply * %) (partition 5 1 (map #(Integer. (str %)) son)))))
 
-  (defn groups [coll]
-    (cond (< (count coll) n) '()
-          :else (cons (take n coll)
-                      (groups (drop n coll)))))
-  (defn generate [i]
-    (cond (>= i n) '()
-          (< (count coll) (+ i n)) '()
-          :else (collect (groups (drop i coll))
-                         (generate (inc i)))))
-  (generate 0))
+(defn problem-8 []
+  (apply
+   max
+   (map max-prdt 
+        (filter #(>= (.length %) 5) 
+                (.split (slurp "../data/problem_8") "[\n0]")))))
 
-(defn cmul [coll]
-  (cond (empty? coll) '()
-        :else (cons (reduce * 1 (map (fn [e] (Integer/parseInt (str e))) (first coll)))
-                    (cmul (rest coll)))))
-
-(time (apply max 
-             (cmul (flatmap 
-                    (fn [l] (all-groups-of 5 l))
-                    (filter (fn [s] (>= (.length s) 5))
-                            (seq (.split n-str "0")))))))
-
+(problem-8)
