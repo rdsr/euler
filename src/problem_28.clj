0d56f44935948d443d2115f5ecd02bd73517cc34
rdsr <rdsr.me@gmail.com>
2010-01-28 15:39:50 +0530

    minor tweaks

--- src/problem_28.clj
+++ src/problem_28.clj
@@ -1,22 +1,25 @@
 (ns problem-28)
 
 (defn- square [x] (* x x))
-(defn odds [] (iterate #(+ % 2) 1))
+(defn- odds [] (iterate #(+ % 2) 1))
 
 (defn problem-28 []
+  "ne specifies a no. in the upper right diagonal
+   nw            ''           ''   left     ''
+   sw            ''          lower  ''      ''    
+   se            ''           ''   right    ''   "
   (let [square-sizes (take 501 (odds))
         ne-nos (fn [] (map #(square %) square-sizes))]
     (apply +
            1 ;; adding it here for the 1x1 square
-           (map (fn [ne sq-sz]
-                  (let [sq-sz-1 (- sq-sz 1)]
+           (map (fn [ne square-size]
+                  (let [square-size-1 (dec square-size)]
                     (+ ne 
-                       (- ne sq-sz-1) 
-                       (- ne (* 2 sq-sz-1))
-                       (- ne (* 3 sq-sz-1)))))
+                       (- ne square-size-1)           ;; nw no
+                       (- ne (* 2 square-size-1))     ;; sw no
+                       (- ne (* 3 square-size-1)))))  ;; se no
                 ;; leaving out 1x1 square
                 (rest (ne-nos)) 
                 (rest square-sizes))))) 
-
 
 (problem-28)
